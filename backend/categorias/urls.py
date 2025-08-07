4c3b3d42ad89aa620823384d91214deb70ee1ea0
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-08-07 17:24:02 -0600

    Se modifico una url y se agrego el metodo delete

--- backend/categorias/urls.py
+++ backend/categorias/urls.py
@@ -5,5 +5,5 @@ from categorias.views import Clase2
 
 urlpatterns = [
     path('categorias', Clase1.as_view()),
-    path('categorias/<int:id>', Clase2.as_view()),
+    path('categorias/<int:id>/', Clase2.as_view()),
 ]
--- backend/categorias/views.py
+++ backend/categorias/views.py
@@ -1,6 +1,7 @@
 # Importaciones
 from django.http.response import JsonResponse
 from django.http import Http404
+from django.utils.text import slugify
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -39,5 +40,33 @@ class Clase2(APIView):
             data = Categoria.objects.filter(id = id).get()
             return JsonResponse({"data": {"id": data.id ,"nombre": data.nombre, "slug": data.slug}},
                              status = HTTPStatus.OK)
+        except Categoria.DoesNotExist:
+            raise Http404
+        
+
+    def put(self, request, id):
+        if request.data.get("nombre") == None or not request.data["nombre"]:
+            return JsonResponse({"estado": "error", "mensaje" : "El campo nombre es obligatorio"},
+                                status = HTTPStatus.BAD_REQUEST)
+        
+        try:
+            data = Categoria.objects.filter(pk = id).get()
+            Categoria.objects.filter(pk = id).update(nombre = request.data.get("nombre"),
+                                    slug = slugify(request.data.get("nombre")))
+            return JsonResponse({"estado": "ok", "mensaje": "Se modifico el registro correctamente"},
+                                 status = HTTPStatus.OK)
+
+        except Categoria.DoesNotExist:
+            raise Http404
+        
+    
+    def delete(self, request, id):
+         
+        try:
+            data = Categoria.objects.filter(pk = id).get()
+            Categoria.objects.filter(pk = id).delete()
+            return JsonResponse({"estado": "ok", "mensaje": "Se elimino el registro correctamente"},
+                                 status = HTTPStatus.OK)
+
         except Categoria.DoesNotExist:
             raise Http404
